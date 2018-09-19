91ba7041c249bd9aee7dc6e15485aef4cda50433
wind-tangkai <wind.tangkai@gmail.com>
2018-09-19 18:54:40 +0800

    regenerate serialVersionUID for BatchModifyEtlMetaStatusResponse

--- src/main/java/com/aliyun/openservices/log/response/BatchModifyEtlMetaStatusResponse.java
+++ src/main/java/com/aliyun/openservices/log/response/BatchModifyEtlMetaStatusResponse.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class BatchModifyEtlMetaStatusResponse extends Response {
 
-    private static final long serialVersionUID = -1056256258768530781L;
+    private static final long serialVersionUID = -2046500800705297648L;
 
     public BatchModifyEtlMetaStatusResponse(Map<String, String> headers) {
         super(headers);
