f5aaae8fe545c816961fc97e84dfb56e6ab7cef7
liketic <ke.like@outlook.com>
2018-10-18 19:27:59 +0800

    Fix intg test

--- src/test/java/com/aliyun/openservices/log/functiontest/JobFunctionTest.java
+++ src/test/java/com/aliyun/openservices/log/functiontest/JobFunctionTest.java
@@ -1,16 +1,14 @@
 package com.aliyun.openservices.log.functiontest;
 
 
-import com.aliyun.openservices.log.common.AlertArguments;
-import com.aliyun.openservices.log.common.AlertResource;
+import com.aliyun.openservices.log.common.AlertConfiguration;
 import com.aliyun.openservices.log.common.EmailNotification;
 import com.aliyun.openservices.log.common.Job;
 import com.aliyun.openservices.log.common.JobSchedule;
-import com.aliyun.openservices.log.common.JobStatus;
+import com.aliyun.openservices.log.common.JobState;
 import com.aliyun.openservices.log.common.JobType;
 import com.aliyun.openservices.log.common.Notification;
 import com.aliyun.openservices.log.common.Query;
-import com.aliyun.openservices.log.common.RetryPolicy;
 import com.aliyun.openservices.log.request.CreateJobRequest;
 import com.aliyun.openservices.log.request.DisableJobRequest;
 import com.aliyun.openservices.log.request.EnableJobRequest;
@@ -18,19 +16,26 @@ import com.aliyun.openservices.log.request.GetJobRequest;
 import com.aliyun.openservices.log.request.ListJobsRequest;
 import com.aliyun.openservices.log.response.GetJobResponse;
 import com.aliyun.openservices.log.response.ListJobsResponse;
-import com.aliyun.openservices.log.util.JsonUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class JobFunctionTest extends FunctionTest {
 
-    private static final String TEST_PROJECT = "project1";
+    private static final String TEST_PROJECT = "project-job-" + getNowTimestamp();
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        client.CreateProject(TEST_PROJECT, "");
+    }
 
     private static String getJobName() {
         return "job-" + getNowTimestamp();
@@ -40,16 +45,14 @@ public class JobFunctionTest extends FunctionTest {
     public void testCrud() throws Exception {
         Job job = new Job();
         String jobName = getJobName();
-        job.setJobName(jobName);
-        job.setType(JobType.Alert);
-        job.setDescription("Job desc");
-        job.setTimeout(120);
-        job.setState(Job.JobState.Enabled);
+        job.setName(jobName);
+        job.setType(JobType.ALERT);
+        job.setDescription("Alert desc");
+        job.setState(JobState.ENABLED);
 
-        AlertArguments arguments = new AlertArguments();
+        AlertConfiguration arguments = new AlertConfiguration();
         arguments.setCondition("$0.count > 1");
-        AlertResource resource = new AlertResource();
-        resource.setDashboard("dashboard1");
+        arguments.setDashboard("dashboard1");
         List<Query> queries = new ArrayList<Query>();
         Query query = new Query();
         query.setPeriod(60);
@@ -57,27 +60,18 @@ public class JobFunctionTest extends FunctionTest {
         query.setLogStore("logStore1");
         query.setChart("chart1");
         queries.add(query);
-        resource.setQueryList(queries);
-        arguments.setResource(resource);
-
+        arguments.setQueryList(queries);
         EmailNotification notification = new EmailNotification();
-        notification.setDashboard("dashboard1");
         notification.setEmailList(Collections.singletonList("kel@test.com"));
         notification.setSubject("Alert fired");
         notification.setContent("Alerting");
         List<Notification> notifications = new ArrayList<Notification>();
         notifications.add(notification);
         arguments.setNotificationList(notifications);
-        job.setArguments(arguments);
-
-        RetryPolicy retryPolicy = new RetryPolicy();
-        retryPolicy.setMaxAttempts(120);
-        retryPolicy.setDeltaBackoff(10);
-        retryPolicy.setType(RetryPolicy.RetryType.Linear);
-        job.setRetryPolicy(retryPolicy);
+        job.setConfiguration(arguments);
 
         JobSchedule schedule = new JobSchedule();
-        schedule.setType(JobSchedule.JobScheduleType.FixedTimeInterval);
+        schedule.setType(JobSchedule.JobScheduleType.FixedRate);
         schedule.setInterval(60L);
         job.setSchedule(schedule);
 
@@ -88,49 +82,38 @@ public class JobFunctionTest extends FunctionTest {
         GetJobResponse response = client.getJob(new GetJobRequest(TEST_PROJECT, jobName));
 
         Job created = response.getJob();
-        assertEquals(created.getJobName(), job.getJobName());
-        assertEquals(created.getType(), JobType.Alert);
-        assertEquals(created.getJobName(), job.getJobName());
+        assertEquals(created.getName(), job.getName());
+        assertEquals(created.getType(), JobType.ALERT);
         assertEquals(created.getDescription(), job.getDescription());
-        assertEquals(created.getTimeout(), job.getTimeout());
         assertEquals(created.getState(), job.getState());
-        assertEquals(created.getArguments(), job.getArguments());
-        job.getSchedule().setDoNotRunUntil(new Date(0));
+        assertEquals(created.getConfiguration(), job.getConfiguration());
         assertEquals(created.getSchedule(), job.getSchedule());
-        assertEquals(created.getLogSetting(), job.getLogSetting());
-        assertEquals(created.getRetryPolicy(), job.getRetryPolicy());
-        assertEquals(created.getStatus(), new JobStatus());
 
         client.disableJob(new DisableJobRequest(TEST_PROJECT, jobName));
         response = client.getJob(new GetJobRequest(TEST_PROJECT, jobName));
         Job job1 = response.getJob();
-        assertEquals(job1.getState(), Job.JobState.Disabled);
+        assertEquals(job1.getState(), JobState.DISABLED);
 
         client.enableJob(new EnableJobRequest(TEST_PROJECT, jobName));
         response = client.getJob(new GetJobRequest(TEST_PROJECT, jobName));
         Job job2 = response.getJob();
-        assertEquals(job2.getState(), Job.JobState.Enabled);
+        assertEquals(job2.getState(), JobState.ENABLED);
 
         // disable for a while
         DisableJobRequest disableForAWhile = new DisableJobRequest(TEST_PROJECT, jobName);
-
-        long now = new Date().getTime();
-        Date doNotRunUntil = new Date(now - (now % 1000));
-        disableForAWhile.setDisableUntil(doNotRunUntil);
         client.disableJob(disableForAWhile);
 
         response = client.getJob(new GetJobRequest(TEST_PROJECT, jobName));
         Job job3 = response.getJob();
 //        System.out.println(JsonUtils.serialize(job3));
-        assertEquals(job3.getState(), Job.JobState.Disabled);
+        assertEquals(job3.getState(), JobState.DISABLED);
 
         JobSchedule schedule1 = job3.getSchedule();
-        assertEquals(schedule1.getDoNotRunUntil(), doNotRunUntil);
         assertEquals(schedule1.getInterval(), schedule.getInterval());
         assertEquals(schedule1.getType(), schedule.getType());
 
         for (int i = 0; i < 10; i++) {
-            job.setJobName("job-" + i);
+            job.setName("job-" + i);
             client.createJob(new CreateJobRequest(TEST_PROJECT, job));
         }
 
@@ -139,7 +122,12 @@ public class JobFunctionTest extends FunctionTest {
         listReq.setOffset(0);
         listReq.setSize(10);
         ListJobsResponse listJobsResponse = client.listJobs(listReq);
-        assertEquals(11, listJobsResponse.getTotal());
-        assertEquals(10, listJobsResponse.getCount());
+        assertEquals(11, (int) listJobsResponse.getTotal());
+        assertEquals(10, (int) listJobsResponse.getCount());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        client.DeleteProject(TEST_PROJECT);
     }
 }
