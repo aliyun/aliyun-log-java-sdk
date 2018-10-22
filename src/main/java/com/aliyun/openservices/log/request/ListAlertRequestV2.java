c45eb113de6713262a0bddaecb2397eae1986ab3
liketic <ke.like@outlook.com>
2018-10-22 10:17:02 +0800

    Remove unused parameters

--- src/main/java/com/aliyun/openservices/log/request/ListAlertRequestV2.java
+++ src/main/java/com/aliyun/openservices/log/request/ListAlertRequestV2.java
@@ -3,35 +3,10 @@ package com.aliyun.openservices.log.request;
 
 import com.aliyun.openservices.log.common.JobType;
 
-import java.util.Map;
-
 
 public class ListAlertRequestV2 extends ListJobsRequest {
 
-    private Boolean fired;
-
     public ListAlertRequestV2(String project) {
         super(project, JobType.ALERT);
-    }
-
-    public ListAlertRequestV2(String project, Boolean fired) {
-        this(project);
-        this.fired = fired;
-    }
-
-    public Boolean isFired() {
-        return fired;
-    }
-
-    public void setFired(Boolean fired) {
-        this.fired = fired;
-    }
-
-    @Override
-    public Map<String, String> GetAllParams() {
-        if (fired != null) {
-            SetParam("fired", fired.toString());
-        }
-        return super.GetAllParams();
     }
 }
