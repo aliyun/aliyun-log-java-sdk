7316a79ddc40b427c953541576b16b85e1bbb7b7
liketic <ke.like@outlook.com>
2018-11-28 10:24:31 +0800

    Support customize end time of relative timespan

--- src/main/java/com/aliyun/openservices/log/common/TimeSpanType.java
+++ src/main/java/com/aliyun/openservices/log/common/TimeSpanType.java
@@ -2,28 +2,24 @@ package com.aliyun.openservices.log.common;
 
 import com.alibaba.fastjson.serializer.JSONSerializable;
 import com.alibaba.fastjson.serializer.JSONSerializer;
-import com.aliyun.openservices.log.util.Args;
 import com.aliyun.openservices.log.util.Utils;
 
 import java.lang.reflect.Type;
 
 public enum TimeSpanType implements JSONSerializable {
+    /**
+     * Predefined relative timespan type. The end is fixed to now.
+     */
     RELATIVE("Relative") {
         @Override
         public void validate(String start, String end, long max) {
-            long d = parseStart(start);
-            if (d > max) {
-                throw new IllegalArgumentException("Timespan must be less than or equal to " + max);
-            }
+            checkRange(parseStart(start), max);
         }
     },
     TRUNCATED("Truncated") {
         @Override
         public void validate(String start, String end, long max) {
-            long d = parseStart(start);
-            if (d > max) {
-                throw new IllegalArgumentException("Timespan must be less than or equal to " + max);
-            }
+            checkRange(parseStart(start), max);
         }
     },
     TODAY("Today") {
@@ -32,14 +28,24 @@ public enum TimeSpanType implements JSONSerializable {
         }
     },
     CUSTOM("Custom") {
+        private long parseEnd(String end) {
+            long now = (System.currentTimeMillis() / 1000);
+            if (end == null || end.isEmpty() || end.equals("now")) {
+                return now;
+            } else if (!end.startsWith("-")) {
+                throw new IllegalArgumentException("Invalid timespan end: " + end);
+            }
+            return Utils.parseDuration(end.substring(1));
+        }
+
         @Override
         public void validate(String start, String end, long max) {
-            if (!"now".equals(end) && !"absolute".equals(end)) {
-                throw new IllegalArgumentException("Invalid timespan end for custom type: " + end);
-            }
-            long d = parseStart(start);
-            if (d > max) {
-                throw new IllegalArgumentException("Timespan must be less than or equal to " + max);
+            if ("absolute".equals(end)) {
+                TRUNCATED.validate(start, end, max);
+            } else {
+                long startAt = parseStart(start);
+                long endAt = parseEnd(end);
+                checkRange(endAt - startAt, max);
             }
         }
     };
@@ -50,10 +56,15 @@ public enum TimeSpanType implements JSONSerializable {
         this.value = value;
     }
 
+    protected void checkRange(long width, long max) {
+        if (width > max) {
+            throw new IllegalArgumentException("Timespan must be less than or equal to " + max);
+        }
+    }
+
     protected long parseStart(String start) {
-        Args.notNullOrEmpty(start, "start");
-        if (!start.startsWith("-")) {
-            throw new IllegalArgumentException("Timespan start must be start with -");
+        if (start == null || !start.startsWith("-")) {
+            throw new IllegalArgumentException("Invalid timespan start: " + start);
         }
         return Utils.parseDuration(start.substring(1));
     }
