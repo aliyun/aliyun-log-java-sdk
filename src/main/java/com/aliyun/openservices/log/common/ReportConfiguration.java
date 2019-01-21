ca58e703fddfda0e4c206ceed656171e889b2510
liketic <ke.like@outlook.com>
2019-01-21 20:59:04 +0800

    Add language as one of report configuration

--- src/main/java/com/aliyun/openservices/log/common/ReportConfiguration.java
+++ src/main/java/com/aliyun/openservices/log/common/ReportConfiguration.java
@@ -5,15 +5,27 @@ import com.aliyun.openservices.log.util.JsonUtils;
 import net.sf.json.JSONObject;
 
 /**
- * Configuration for report job.
+ * Configuration for report.
  */
 public class ReportConfiguration extends DashboardBasedJobConfiguration {
 
+    /**
+     * Whether add watermark on image, default to true.
+     */
     @JSONField
     private boolean enableWatermark;
 
+    /**
+     * Whether create a public access url for dashboard, default to false.
+     */
     @JSONField
     private boolean createPublicAccessUrl;
+
+    /**
+     * Optional language for internationalization. Defaults as zh.
+     */
+    @JSONField
+    private String language;
 
     public boolean getEnableWatermark() {
         return enableWatermark;
@@ -29,6 +41,14 @@ public class ReportConfiguration extends DashboardBasedJobConfiguration {
 
     public void setCreatePublicAccessUrl(boolean createPublicAccessUrl) {
         this.createPublicAccessUrl = createPublicAccessUrl;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
     }
 
     @Override
@@ -48,6 +68,7 @@ public class ReportConfiguration extends DashboardBasedJobConfiguration {
         super.deserialize(value);
         enableWatermark = JsonUtils.readBool(value, "enableWatermark", false);
         createPublicAccessUrl = JsonUtils.readBool(value, "createPublicAccessUrl", false);
+        language = JsonUtils.readOptionalString(value, "language");
     }
 
     @Override
@@ -58,13 +79,15 @@ public class ReportConfiguration extends DashboardBasedJobConfiguration {
         ReportConfiguration that = (ReportConfiguration) o;
 
         if (getEnableWatermark() != that.getEnableWatermark()) return false;
-        return getCreatePublicAccessUrl() == that.getCreatePublicAccessUrl();
+        if (getCreatePublicAccessUrl() != that.getCreatePublicAccessUrl()) return false;
+        return getLanguage() != null ? getLanguage().equals(that.getLanguage()) : that.getLanguage() == null;
     }
 
     @Override
     public int hashCode() {
         int result = (getEnableWatermark() ? 1 : 0);
         result = 31 * result + (getCreatePublicAccessUrl() ? 1 : 0);
+        result = 31 * result + (getLanguage() != null ? getLanguage().hashCode() : 0);
         return result;
     }
 }
