f989a1e7cf3d255ab086f21ce4a309c31c1977b2
liketic <ke.like@outlook.com>
2019-01-31 20:37:40 +0800

    Remove dead code

--- src/main/java/com/aliyun/openservices/log/request/UpdateDashboardRequest.java
+++ src/main/java/com/aliyun/openservices/log/request/UpdateDashboardRequest.java
@@ -5,7 +5,7 @@ import com.aliyun.openservices.log.common.Dashboard;
 public class UpdateDashboardRequest extends Request {
 
 	private static final long serialVersionUID = -7340328426947680281L;
-	private Dashboard dashboard = new Dashboard();
+	private Dashboard dashboard;
 
 	public UpdateDashboardRequest(String project, Dashboard dashboard) {
 		super(project);
