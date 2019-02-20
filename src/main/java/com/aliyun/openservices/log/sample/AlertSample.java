c4eb4e434bdaddf318f05cfca8f8bab4f2b41682
liketic <ke.like@outlook.com>
2019-02-20 19:21:45 +0800

    Update alert sample

--- src/main/java/com/aliyun/openservices/log/sample/AlertSample.java
+++ src/main/java/com/aliyun/openservices/log/sample/AlertSample.java
@@ -1,8 +1,8 @@
 package com.aliyun.openservices.log.sample;
 
 import com.aliyun.openservices.log.Client;
-import com.aliyun.openservices.log.common.AlertConfiguration;
 import com.aliyun.openservices.log.common.Alert;
+import com.aliyun.openservices.log.common.AlertConfiguration;
 import com.aliyun.openservices.log.common.Chart;
 import com.aliyun.openservices.log.common.Dashboard;
 import com.aliyun.openservices.log.common.DingTalkNotification;
@@ -16,17 +16,14 @@ import com.aliyun.openservices.log.common.TimeSpanType;
 import com.aliyun.openservices.log.exception.LogException;
 import com.aliyun.openservices.log.request.CreateAlertRequest;
 import com.aliyun.openservices.log.request.CreateDashboardRequest;
-import com.aliyun.openservices.log.request.DeleteAlertRequest;
-import com.aliyun.openservices.log.request.DeleteDashboardRequest;
 import com.aliyun.openservices.log.request.DisableAlertRequest;
 import com.aliyun.openservices.log.request.EnableAlertRequest;
 import com.aliyun.openservices.log.request.GetAlertRequest;
 import com.aliyun.openservices.log.request.ListAlertRequest;
-import com.aliyun.openservices.log.request.ListDashboardRequest;
 import com.aliyun.openservices.log.request.UpdateAlertRequest;
 import com.aliyun.openservices.log.response.GetAlertResponse;
 import com.aliyun.openservices.log.response.ListAlertResponse;
-import com.aliyun.openservices.log.response.ListDashboardResponse;
+import net.sf.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -42,42 +39,65 @@ public class AlertSample {
         String project = "ali-cn-devcommon-sls-admin";
         String host = "cn-hangzhou-devcommon-intranet.sls.aliyuncs.com";
         String alertName = "test-alert";
+        String dashboardName = "dashboardtest";
+        String logstore = "logstore-test";
 
         Client client = new Client(host, accessId, accessKey);
         try {
-            // test list jobs
-            ListAlertRequest listReq = new ListAlertRequest(project);
-            listReq.setOffset(0);
-            listReq.setSize(100);
-            ListAlertResponse listJobsResponse = client.listAlert(listReq);
-            for (Alert item : listJobsResponse.getResults()) {
-                client.deleteAlert(new DeleteAlertRequest(project, item.getName()));
-            }
-            ListDashboardRequest listDashboardRequest = new ListDashboardRequest(project);
-            listDashboardRequest.setSize(100);
-            listDashboardRequest.setOffset(0);
-            ListDashboardResponse listDashboardResponse = client.listDashboard(listDashboardRequest);
-            for (Dashboard dashboard : listDashboardResponse.getDashboards()) {
-                client.deleteDashboard(new DeleteDashboardRequest(project, dashboard.getDashboardName()));
+            // create a dashboard first
+            Dashboard dashboard = new Dashboard();
+            dashboard.setDashboardName(dashboardName);
+            dashboard.setDescription("Dashboard");
+
+            // add chart to dashboard
+            Chart chart = new Chart();
+            chart.setDisplayName("chart-test");
+            chart.setQuery("* | select count(1) as count");
+            chart.setLogstore(logstore);
+            chart.setTitle("chart-1234567");
+            chart.setType("table");
+            chart.setTopic("");
+            chart.setHeight(5);
+            chart.setWidth(5);
+            chart.setStart("-360s");
+            chart.setEnd("now");
+            chart.setxPosition(0);
+            chart.setyPosition(-1);
+            JSONObject searchAttr = new JSONObject();
+            searchAttr.put("logstore", logstore);
+            searchAttr.put("start", chart.getStart());
+            searchAttr.put("end", chart.getEnd());
+            searchAttr.put("topic", "");
+            searchAttr.put("query", chart.getQuery());
+            searchAttr.put("timeSpanType", "custom");
+            chart.setRawSearchAttr(searchAttr.toString());
+            ArrayList<Chart> charts = new ArrayList<Chart>();
+            charts.add(chart);
+            dashboard.setChartList(charts);
+            CreateDashboardRequest createDashboardRequest = new CreateDashboardRequest(project, dashboard);
+            try {
+                client.createDashboard(createDashboardRequest);
+            } catch (LogException ex) {
+                if (!ex.GetErrorMessage().equals("specified dashboard already exists")) {
+                    throw ex;
+                }
             }
             Alert alert = new Alert();
             alert.setName(alertName);
             alert.setState(JobState.ENABLED);
             alert.setDisplayName("count monitoring");
-
             AlertConfiguration configuration = new AlertConfiguration();
             configuration.setCondition("count > 1");
-
             // The name of dashboard this alert associated to
-            configuration.setDashboard("dashboardtest");
+            configuration.setDashboard(dashboardName);
             List<Query> queries = new ArrayList<Query>();
             Query query = new Query();
-            query.setStart("-60s");
+            query.setStart("-360s");
             query.setEnd("now");
             query.setTimeSpanType(TimeSpanType.CUSTOM);
-            query.setQuery("* | select count(1) as count");
-            query.setLogStore("logStore1");
-            query.setChartTitle("chart1");
+            query.setQuery(chart.getQuery());
+            query.setLogStore(logstore);
+            query.setChartTitle(chart.getTitle());
             queries.add(query);
             configuration.setQueryList(queries);
 
@@ -105,17 +125,9 @@ public class AlertSample {
             schedule.setType(JobScheduleType.FIXED_RATE);
             schedule.setInterval("60s");
             alert.setSchedule(schedule);
+            client.createAlert(new CreateAlertRequest(project, alert));
 
-            // alert must be associated with a dashboard
-            Dashboard dashboard = new Dashboard();
-            dashboard.setDashboardName("dashboardtest");
-            dashboard.setDescription("Dashboard");
-            dashboard.setChartList(new ArrayList<Chart>());
-            CreateDashboardRequest createDashboardRequest = new CreateDashboardRequest(project, dashboard);
-            client.createDashboard(createDashboardRequest);
-            CreateAlertRequest request = new CreateAlertRequest(project, alert);
-            client.createAlert(request);
-
+            // Get alert rule by name
             GetAlertResponse response = client.getAlert(new GetAlertRequest(project, alertName));
 
             Alert created = response.getAlert();
@@ -147,18 +159,20 @@ public class AlertSample {
             Date muteTo = new Date(System.currentTimeMillis() + 60 * 1000);
             alert3.getConfiguration().setMuteUntil(muteTo);
             client.updateAlert(new UpdateAlertRequest(project, alert3));
-            response = client.getAlert(new GetAlertRequest(project, alertName));
             for (int i = 0; i < 10; i++) {
                 alert.setName("alert-" + i);
                 client.createAlert(new CreateAlertRequest(project, alert));
             }
             // test list jobs
-            listReq = new ListAlertRequest(project);
+            ListAlertRequest listReq = new ListAlertRequest(project);
             listReq.setOffset(0);
             listReq.setSize(10);
-            listJobsResponse = client.listAlert(listReq);
+            ListAlertResponse listJobsResponse = client.listAlert(listReq);
             System.out.println(listJobsResponse.getTotal());
             System.out.println(listJobsResponse.getCount());
+            for (Alert alert4 : listJobsResponse.getResults()) {
+                System.out.println(alert4.getName());
+            }
         } catch (LogException lex) {
             lex.printStackTrace();
         }
