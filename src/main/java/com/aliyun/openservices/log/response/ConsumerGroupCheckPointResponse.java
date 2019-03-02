194b794c5b9dd3fdad1b114eec985bdcf0de5d90
Like <like_0903@163.com>
2019-03-02 20:42:37 +0800

    Use interface instead of implementation in consumer request and response (#60)

--- src/main/java/com/aliyun/openservices/log/response/ConsumerGroupCheckPointResponse.java
+++ src/main/java/com/aliyun/openservices/log/response/ConsumerGroupCheckPointResponse.java
@@ -1,6 +1,7 @@
 package com.aliyun.openservices.log.response;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import net.sf.json.JSONArray;
@@ -8,30 +9,44 @@ import net.sf.json.JSONArray;
 import com.aliyun.openservices.log.common.ConsumerGroupShardCheckPoint;
 
 public class ConsumerGroupCheckPointResponse extends Response {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8040754126341011292L;
-	private ArrayList<ConsumerGroupShardCheckPoint> checkPoints;
-	public ConsumerGroupCheckPointResponse(Map<String, String> headers, JSONArray checkPointsArray) {
-		super(headers);
-		checkPoints = new ArrayList<ConsumerGroupShardCheckPoint>();
-		for(int i = 0; i < checkPointsArray.size(); ++i)
-		{
-			ConsumerGroupShardCheckPoint checkpoint = new ConsumerGroupShardCheckPoint();
-			checkpoint.Deserialize(checkPointsArray.getJSONObject(i));
-			checkPoints.add(checkpoint);
-		}
-	}
-	/**
-	 * @return
-	 * 		shard checkpoint
-	 */
-	public ArrayList<ConsumerGroupShardCheckPoint> GetCheckPoints() {
-		return checkPoints;
-	}
-	public void SetCheckPoints(ArrayList<ConsumerGroupShardCheckPoint> checkPoints) {
-		this.checkPoints = checkPoints;
-	}
-	
+    /**
+     *
+     */
+    private static final long serialVersionUID = 8040754126341011292L;
+    private List<ConsumerGroupShardCheckPoint> checkPoints;
+
+    public ConsumerGroupCheckPointResponse(Map<String, String> headers, JSONArray checkPointsArray) {
+        super(headers);
+        checkPoints = new ArrayList<ConsumerGroupShardCheckPoint>();
+        for (int i = 0; i < checkPointsArray.size(); ++i) {
+            ConsumerGroupShardCheckPoint checkpoint = new ConsumerGroupShardCheckPoint();
+            checkpoint.Deserialize(checkPointsArray.getJSONObject(i));
+            checkPoints.add(checkpoint);
+        }
+    }
+
+    /**
+     * @return shard checkpoint
+     * @deprecated Use {@link #getCheckPoints()} instead.
+     */
+    @Deprecated
+    public ArrayList<ConsumerGroupShardCheckPoint> GetCheckPoints() {
+        return new ArrayList<ConsumerGroupShardCheckPoint>(checkPoints);
+    }
+
+    /**
+     * @deprecated Use {@link #setCheckPoints(List)} instead.
+     */
+    @Deprecated
+    public void SetCheckPoints(ArrayList<ConsumerGroupShardCheckPoint> checkPoints) {
+        this.checkPoints = checkPoints;
+    }
+
+    public List<ConsumerGroupShardCheckPoint> getCheckPoints() {
+        return checkPoints;
+    }
+
+    public void setCheckPoints(List<ConsumerGroupShardCheckPoint> checkPoints) {
+        this.checkPoints = checkPoints;
+    }
 }
--- src/main/java/com/aliyun/openservices/log/response/ConsumerGroupHeartBeatResponse.java
+++ src/main/java/com/aliyun/openservices/log/response/ConsumerGroupHeartBeatResponse.java
@@ -1,27 +1,47 @@
 package com.aliyun.openservices.log.response;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ConsumerGroupHeartBeatResponse extends Response {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4941087094480475965L;
-	private ArrayList<Integer> shards;
-	public ConsumerGroupHeartBeatResponse(Map<String, String> headers, ArrayList<Integer> shards) {
-		super(headers);
-		this.shards = shards;
-	}
-	/**
-	 * @return
-	 * 		the shards consumer should held in time
-	 */
-	public ArrayList<Integer> GetShards() {
-		return shards;
-	}
-	public void SetShards(ArrayList<Integer> shards) {
-		this.shards = shards;
-	}
-	
+    private static final long serialVersionUID = 4941087094480475965L;
+    private List<Integer> shards;
+
+    public ConsumerGroupHeartBeatResponse(Map<String, String> headers, List<Integer> shards) {
+        super(headers);
+        this.shards = shards;
+    }
+
+    /**
+     * @deprecated Use {@link #ConsumerGroupHeartBeatResponse(Map, List)} instead.
+     */
+    @Deprecated
+    public ConsumerGroupHeartBeatResponse(Map<String, String> headers, ArrayList<Integer> shards) {
+        super(headers);
+        this.shards = shards;
+    }
+
+    /**
+     * @return the shards consumer should held in time
+     * @deprecated Use {@link #getShards()} instead.
+     */
+    public ArrayList<Integer> GetShards() {
+        return new ArrayList<Integer>(shards);
+    }
+
+    /**
+     * @deprecated Use {@link #setShards(List)} instead.
+     */
+    public void SetShards(ArrayList<Integer> shards) {
+        this.shards = shards;
+    }
+
+    public List<Integer> getShards() {
+        return shards;
+    }
+
+    public void setShards(List<Integer> shards) {
+        this.shards = shards;
+    }
 }
