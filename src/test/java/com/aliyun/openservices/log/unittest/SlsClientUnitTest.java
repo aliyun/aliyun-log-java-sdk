b44270f9471a3d79b62f3efb535ec1474ff76c2e
liketic <ke.like@outlook.com>
2019-03-03 10:29:25 +0800

    Remove unused import

--- src/test/java/com/aliyun/openservices/log/unittest/SlsClientUnitTest.java
+++ src/test/java/com/aliyun/openservices/log/unittest/SlsClientUnitTest.java
@@ -10,7 +10,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URI;
