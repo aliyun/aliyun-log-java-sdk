e16404ff5dac7d517a25849f7a9427bbcfaa026d
Like <like_0903@163.com>
2019-03-21 22:46:21 +0800

    Remove chinese comments (#62)

--- src/main/java/com/aliyun/openservices/log/exception/LogException.java
+++ src/main/java/com/aliyun/openservices/log/exception/LogException.java
@@ -4,115 +4,109 @@
 package com.aliyun.openservices.log.exception;
 
 /**
- * The excpetion is thrown if error happen.
- * 
+ * The exception is thrown if error happen.
+ *
  * @author sls_dev
- * 
  */
 public class LogException extends Exception {
 
-	private static final long serialVersionUID = -4441995860203577032L;
+    private static final long serialVersionUID = -4441995860203577032L;
 
-	private int httpCode = -1;
+    private int httpCode = -1;
 
-	private String errorCode;
+    private String errorCode;
 
-	private String requestId;
+    private String requestId;
 
-	/**
-	 * Construct LogException
-	 * 
-	 * @param code
-	 *            error code
-	 * @param message
-	 *            error message
-	 * @param requestId
-	 *            request id from sls server, if the error is happened in the
-	 *            client, the request id is empty
-	 */
-	public LogException(String code, String message, String requestId) {
-		super(message);
-		this.errorCode = code;
-		this.requestId = requestId;
-	}
+    /**
+     * Construct LogException
+     *
+     * @param code      error code
+     * @param message   error message
+     * @param requestId request id from sls server, if the error is happened in the
+     *                  client, the request id is empty
+     */
+    public LogException(String code, String message, String requestId) {
+        super(message);
+        this.errorCode = code;
+        this.requestId = requestId;
+    }
 
-	/**
-	 * Construct LogException
-	 * 
-	 * @param code
-	 *            error code
-	 * @param message
-	 *            error message
-	 * @param cause
-	 *            inner exception, which cause the error
-	 * @param requestId
-	 *            request id from sls server, if the error is happened in the
-	 *            client, the request id is empty
-	 */
-	public LogException(String code, String message, Throwable cause,
-			String requestId) {
-		super(message, cause);
-		this.errorCode = code;
-		this.requestId = requestId;
-	}
+    /**
+     * Construct LogException
+     *
+     * @param code      error code
+     * @param message   error message
+     * @param cause     inner exception, which cause the error
+     * @param requestId request id from sls server, if the error is happened in the
+     *                  client, the request id is empty
+     */
+    public LogException(String code, String message, Throwable cause,
+                        String requestId) {
+        super(message, cause);
+        this.errorCode = code;
+        this.requestId = requestId;
+    }
 
-	/**
-	 * Construct LogException
-	 *
-	 * @param httpCode http code, -1 the error is happened in the client
-	 * @param code error code
-	 * @param message error message
-	 * @param requestId request id from sls server, if the error is happened in the
-	 *                  client, the request id is empty
-	 */
-	public LogException(int httpCode, String code, String message, String requestId) {
-		super(message);
-		this.httpCode = httpCode;
-		this.errorCode = code;
-		this.requestId = requestId;
-	}
+    /**
+     * Construct LogException
+     *
+     * @param httpCode  http code, -1 the error is happened in the client
+     * @param code      error code
+     * @param message   error message
+     * @param requestId request id from sls server, if the error is happened in the
+     *                  client, the request id is empty
+     */
+    public LogException(int httpCode, String code, String message, String requestId) {
+        super(message);
+        this.httpCode = httpCode;
+        this.errorCode = code;
+        this.requestId = requestId;
+    }
 
-	/**
-	 * Get the error code
-	 * 
-	 * @return error code
-	 */
-	public String GetErrorCode() {
-		return this.errorCode;
-	}
+    /**
+     * Get the error code
+     *
+     * @return error code
+     */
+    public String GetErrorCode() {
+        return this.errorCode;
+    }
 
-	/**
-	 * Get the error message
-	 * 
-	 * @return error message
-	 */
-	public String GetErrorMessage() {
-		return super.getMessage();
-	}
+    /**
+     * Get the error message
+     *
+     * @return error message
+     */
+    public String GetErrorMessage() {
+        return super.getMessage();
+    }
 
-	/**
-	 * Get the request id
-	 * 
-	 * @return request id, if the error is happened in the client, the request
-	 *         id is empty
-	 */
-	public String GetRequestId() {
-		return this.requestId;
-	}
+    /**
+     * Get the request id
+     *
+     * @return request id, if the error is happened in the client, the request
+     * id is empty
+     */
+    public String GetRequestId() {
+        return this.requestId;
+    }
 
-	/**
-	 * Get the http response code
-	 * @return http code, -1 the error is happened in the client
-	 */
-	public int GetHttpCode() {
-		return httpCode;
-	}
+    /**
+     * Get the http response code
+     *
+     * @return http code, -1 the error is happened in the client
+     */
+    public int GetHttpCode() {
+        return httpCode;
+    }
 
-	/**
-	 * Set the http response code
-	 * @param httpCode http code, -1 the error is happened in the client
-	 */
-	public void SetHttpCode(int httpCode) {
-		this.httpCode = httpCode;
-	}
+    /**
+     * Set the http response code
+     *
+     * @param httpCode http code, -1 the error is happened in the client
+     */
+    public void SetHttpCode(int httpCode) {
+        this.httpCode = httpCode;
+    }
 }
