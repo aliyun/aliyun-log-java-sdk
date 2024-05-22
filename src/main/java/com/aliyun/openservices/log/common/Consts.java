9f50d77982959a1eca9880b9d2cba7c6fa159dd6
士宁 <shining.wz@alibaba-inc.com>
2024-05-22 16:06:57 +0800

    1. add reverse param for GetHistogramsRequest 2. add scanBytes for GetHistogramsResponse(only valid for spl pushdown query)

--- src/main/java/com/aliyun/openservices/log/common/Consts.java
+++ src/main/java/com/aliyun/openservices/log/common/Consts.java
@@ -103,6 +103,7 @@ public class Consts {
     public static final String CONST_X_SLS_CURSOR = "x-log-cursor";
     public static final String CONST_X_SLS_COUNT = "x-log-count";
     public static final String CONST_X_SLS_PROCESS = "x-log-progress";
+    public static final String CONST_X_SLS_SCANBYTES = "x-log-scanbytes";
     public static final String CONST_X_SLS_NEXT_TOKEN = "x-log-nexttoken";
     public static final String CONST_X_ACS_SECURITY_TOKEN = "x-acs-security-token";
     public static final String CONST_X_LOG_RESOURCEOWNERACCOUNT = "x-log-resourceowneraccount";
