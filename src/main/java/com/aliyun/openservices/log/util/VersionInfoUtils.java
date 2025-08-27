d34a7eade4a00b5db21a8e50c095f548099dd62f
crimson <1291463831@qq.com>
2025-08-27 19:46:07 +0800

    feat: remove version info file

--- src/main/java/com/aliyun/openservices/log/util/VersionInfoUtils.java
+++ src/main/java/com/aliyun/openservices/log/util/VersionInfoUtils.java
@@ -7,19 +7,9 @@ import java.util.Properties;
  * Unit test for VersionInfo.
  */
 public class VersionInfoUtils {
-    private static final String VERSION_INFO_FILE = "versioninfo.properties";
     private static final String USER_AGENT_PREFIX = "aliyun-log-sdk-java";
 
-    private static String version = null;
-
     private static String defaultUserAgent = null;
-
-    public static String getVersion() {
-        if (version == null) {
-            initializeVersion();
-        }
-        return version;
-    }
 
     public static String getDefaultUserAgent() {
         if (defaultUserAgent == null) {
@@ -30,25 +20,11 @@ public class VersionInfoUtils {
                 version = pkg.getImplementationVersion();
             }
             if (version == null) {
-                version = getVersion();
+                version = "unknown-version";
             }
             defaultUserAgent = USER_AGENT_PREFIX + "-" + version + "/" + System.getProperty("java.version");
         }
         return defaultUserAgent;
     }
 
-    private static void initializeVersion() {
-        InputStream inputStream = VersionInfoUtils.class.getClassLoader().getResourceAsStream(VERSION_INFO_FILE);
-        Properties versionInfoProperties = new Properties();
-        try {
-            if (inputStream == null) {
-                throw new IllegalArgumentException(VERSION_INFO_FILE + " not found on classpath");
-            }
-
-            versionInfoProperties.load(inputStream);
-            version = versionInfoProperties.getProperty("version");
-        } catch (Exception e) {
-            version = "unknown-version";
-        }
-    }
 }
