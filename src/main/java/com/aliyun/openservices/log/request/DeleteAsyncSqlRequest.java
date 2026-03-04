c96dce11b120b5c36767fd58b639b2e63c0264d0
crimson <1291463831@qq.com>
2026-03-04 09:42:12 +0800

    feat: add logstore into Get/Delete AsyncSql request, bump version 0.6.152
    Add logstore field to DeleteAsyncSqlRequest and GetAsyncSqlRequest classes,
    update constructors to include logstore parameter, and update sample code accordingly.

--- src/main/java/com/aliyun/openservices/log/request/DeleteAsyncSqlRequest.java
+++ src/main/java/com/aliyun/openservices/log/request/DeleteAsyncSqlRequest.java
@@ -1,14 +1,26 @@
 package com.aliyun.openservices.log.request;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DeleteAsyncSqlRequest extends Request {
+    private final String logstore;
     private final String queryId;
 
-    public DeleteAsyncSqlRequest(String project, String queryId) {
+    public DeleteAsyncSqlRequest(String project, String logstore, String queryId) {
         super(project);
+        this.logstore = logstore;
         this.queryId = queryId;
     }
 
     public String getQueryId() {
         return queryId;
+    }
+
+    @Override
+    public Map<String, String> GetAllParams() {
+        Map<String, String> params = new HashMap<>();
+        params.put("logstore", logstore);
+        return params;
     }
 }
